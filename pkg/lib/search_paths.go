95f016060083ca29c65e98b4c3172ad286c1ab34
mengz <2567587994@qq.com>
2026-06-26 09:20:35 +0800

    document mxsml library search order
    ?? API ?? 2 ??????

--- pkg/lib/search_paths.go
+++ pkg/lib/search_paths.go
@@ -2,6 +2,8 @@ package lib
 
 const mxsmlLibName = "libmxsml.so"
 
+// CandidateLibraryPaths returns the candidate absolute paths used to locate
+// libmxsml.so after Load() has already tried the system loader search path.
 func CandidateLibraryPaths() []string {
 	return []string{
 		"/opt/mxdriver/lib/" + mxsmlLibName,
